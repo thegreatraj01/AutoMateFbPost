d7f61f8a353d221639294ca2b09e6441503a08f9
thegreatraj01 <thegreatraj0810@gmail.com>
2025-05-20 21:28:30 +0530

    removed settimeout from health check

--- server/src/controllers/healthcheck.controller.js
+++ server/src/controllers/healthcheck.controller.js
@@ -4,17 +4,15 @@ import { HTTP_STATUS_CODES } from "../utils/HttpStatusCode.js";
 
 // DONE :
 const healthcheck = asyncHandler(async (req, res) => {
-    setTimeout(() => {
-        return res
-            .status(200)
-            .json(
-                new ApiResponse(
-                    HTTP_STATUS_CODES.OK.code,
-                    { status: "OK" },
-                    "Healthcheck passed successfully"
-                )
-            );
-    }, 5000);
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                HTTP_STATUS_CODES.OK.code,
+                { status: "OK" },
+                "Healthcheck passed successfully"
+            )
+        );
 });
 
 export { healthcheck };
