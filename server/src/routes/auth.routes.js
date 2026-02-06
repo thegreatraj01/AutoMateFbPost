1650757e5cbe897fab7a40b6938f34b061a3ad76
thegreatraj01 <thegreatraj0810@gmail.com>
2026-02-06 23:55:29 +0530

    change loder , update readme.md and otp is not working on render , render is blocking port in free tier

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,40 +1,42 @@
 import express from "express";
-import { loginUser, registerUser, resendEmailVerificationOtp, verifyEmail, resetPassword ,sendPasswordResetOtp, logoutUser } from "../controllers/auth.controller.js";
+import {
+  loginUser,
+  registerUser,
+  resendEmailVerificationOtp,
+  verifyEmail,
+  resetPassword,
+  sendPasswordResetOtp,
+  logoutUser,
+} from "../controllers/auth.controller.js";
 import { otpRequestLimiter } from "../middleware/rateLimter.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const authRouter = express.Router();
 
-
-// Register User First Time 
+// Register User First Time
 authRouter.post("/register", registerUser);
 
 // Verify User Email
-authRouter.post('/verify-email', verifyEmail);
+authRouter.post("/verify-email", verifyEmail);
 
 // Resend OTP to Email for email verification
-authRouter.post('/otp-request', otpRequestLimiter, resendEmailVerificationOtp);
+authRouter.post("/otp-request", otpRequestLimiter, resendEmailVerificationOtp);
 
 // login User
 authRouter.post("/login", loginUser);
 
-// Send otp to reset password 
+// Send otp to reset password
 authRouter.post("/send-p-reset-otp", otpRequestLimiter, sendPasswordResetOtp);
 
-// Reset Password 
+// Reset Password
 authRouter.post("/reset-password", resetPassword);
 // Logout User
 authRouter.post("/logout", verifyJWT, logoutUser);
 
 export default authRouter;
 
-
-
-
-
 // // Route: /auth/facebook
 // authRouter.get('/facebook', authFacebook);
 // // Route: /auth/facebook/callback
 // authRouter.get("/facebook/callback", facebookCallback);
 // Route: /auth/logout
-
